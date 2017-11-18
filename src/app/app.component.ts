5739291f0b3e869d7e3c9479323c3a4b267dacc3
Kallahan23 <armkalnian@gmail.com>
2017-11-18 14:31:51 +1100

    Status bar colour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,9 @@ export class MyApp {
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
-            // statusBar.styleDefault();
             if (platform.is("cordova")) {
-                statusBar.backgroundColorByHexString('488aff');
+                statusBar.styleDefault();
+                // statusBar.backgroundColorByHexString('488aff');
                 splashScreen.hide();
             }
         });

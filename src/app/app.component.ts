c21fb7327401a25977859bec95bd15ff5b184615
Kallahan23 <armkalnian@gmail.com>
2018-04-29 18:40:24 +1000

    Status bar bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class MyApp {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             if (platform.is("cordova")) {
-                statusBar.styleDefault();
+                // statusBar.styleDefault();
                 statusBar.backgroundColorByHexString('488aff');
                 splashScreen.hide();
             }
